06d5bfc2a547c1e1da69a9c846d171b2a54f9463
Alejandroh2000 <alejandroh2000@outlook.com>
2022-11-03 15:17:51 -0600

    03/11

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -21,6 +21,8 @@ export class GifsService {
   constructor(private http: HttpClient) {
 
     this._historial= JSON.parse( localStorage.getItem('historial')!) || []
+    this.resultados= JSON.parse( localStorage.getItem('resultados')!) || []
+
     //  if(localStorage.getItem('historial') ){
   //   this._historial= JSON.parse( localStorage.getItem('historial')!);
   //  }
@@ -35,22 +37,22 @@ export class GifsService {
     query = query.trim().toLocaleLowerCase();
 
     if(!this._historial.includes(query)){
-    //  this._historial.unshift( query );
-    //  this._historial = this._historial.splice(0, 10)
+      this._historial.unshift( query );
+      this._historial = this._historial.splice(0, 10)
 
       localStorage.setItem('historial', JSON.stringify(this._historial) );
-
+      
     }
     
 
     
     console.log(this._historial)
   
-    this.http.get<SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=pAUJo7fPKSF5XxaJ50NloT775xv8NvVt&q=${query}&limit=10`)
+    this.http.get<SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=pAUJo7fPKSF5XxaJ50NloT775xv8NvVt&q=${query}&limit=30`)
     .subscribe( (resp ) => {
       console.log(resp.data);
       this.resultados = resp.data;
-      
+      localStorage.setItem('resultados', JSON.stringify(this.resultados) );
     });
   
   }
